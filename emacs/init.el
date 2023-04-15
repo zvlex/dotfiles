8cf15223f80f122171dd16e91ed14a8bc535e46e
Alexander Zutikov <zvlex.dev@gmail.com>
2023-04-15 15:35:47 +0400

    Update Emacs configuration

---
+++ emacs/init.el
@@ -0,0 +1,151 @@
+(require 'package)
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+(package-initialize)
+
+(require 'eglot)
+(require 'elixir-mode)
+(add-hook 'elixir-mode-hook 'eglot-ensure)
+(add-to-list 'eglot-server-programs '(elixir-mode "~/.emacs.d/pkg/elixir-ls/language_server.sh"))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(global-visual-line-mode t)
+ '(package-selected-packages
+   '(try diff-hl dotenv-mode multiple-cursors treemacs-magit yaml-mode move-text projectile-rails treemacs-projectile which-key projectile lsp-mode mentor whitespace-cleanup-mode magit smartparens company-ctags company ivy elixir-mode eglot)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+
+;; Emacs
+(require 'use-package)
+(global-set-key [remap list-buffers] 'ibuffer)
+(global-display-line-numbers-mode t)
+(setq ring-bell-function 'ignore)
+
+(tool-bar-mode -1)
+
+(defun set-eshell-path-from-asdf ()
+  "Set eshell's PATH from asdf's environment variables."
+  (let ((shims-path (concat (getenv "HOME") "/.asdf/shims")))
+    (setq eshell-path-env (concat shims-path ":" eshell-path-env))))
+
+(add-hook 'eshell-mode-hook #'set-eshell-path-from-asdf)
+
+;; Disable extra files
+(setq make-backup-files         nil) ; Don't want any backup files
+(setq auto-save-list-file-name  nil) ; Don't want any .saves files
+(setq auto-save-default         nil) ; Don't want any auto saving
+(setq require-final-newline t)
+
+;; Clear Whitespace
+(require 'whitespace-cleanup-mode)
+(global-whitespace-cleanup-mode)
+(add-hook 'before-save-hook 'whitespace-cleanup)
+
+;; Ido
+(setq ido-enable-flex-matching t)
+(setq ido-everywhere t)
+(ido-mode 1)
+
+;; Autocomplete
+(use-package company
+  :ensure t
+  :init
+  (add-hook 'after-init-hook 'global-company-mode))
+
+;; Autopair
+(require 'smartparens-config)
+(use-package smartparens-mode
+  :ensure smartparens
+  :hook (prog-mode text-mode markdown-mode)
+  :init
+  (require 'smartparens-config)
+)
+
+;; LSP
+(require 'lsp-mode)
+(add-to-list 'exec-path "/home/zvlex/.asdf/shims/")
+(setq lsp-ruby-executable "/home/zvlex/.asdf/shims/ruby-lsp")
+(add-hook 'ruby-mode-hook #'lsp)
+
+;; Project
+(use-package projectile
+  :ensure t
+  :init
+  (projectile-mode +1)
+  :bind-keymap
+	 ("s-p" . projectile-command-map)
+	 ("C-c p" . projectile-command-map)
+  :config
+  (setq projectile-switch-project-action #'projectile-treemacs)
+  (defun projectile-treemacs ()
+    (interactive)
+    (projectile-dired)
+    (treemacs)))
+
+(use-package treemacs-projectile
+  :after (treemacs projectile)
+  :ensure t)
+
+(use-package projectile-rails
+  :ensure t
+  :after projectile
+  :bind-keymap
+  ("C-c r" . projectile-rails-command-map)
+  :config
+  (projectile-rails-global-mode))
+
+(use-package which-key
+  :ensure t
+  :config (which-key-mode))
+
+(use-package treemacs
+  :ensure t
+  :bind
+  (:map global-map
+	([f8] . treemacs))
+  :config
+  (progn
+   (setq treemacs-is-never-other-window t)))
+
+(use-package treemacs-magit
+  :after (treemacs magit)
+  :ensure t)
+
+(use-package move-text
+  :ensure t
+  :config (move-text-default-bindings))
+
+(use-package yaml-mode
+  :ensure t
+  :mode "\\.ya?ml\\'")
+
+(use-package multiple-cursors
+  :ensure t
+  :bind (("C-c m c" . mc/edit-lines)
+	 ("C-c m n" . mc/mark-next-like-this)
+	 ("C-c m p" . mc/mark-previous-like-this)
+	 ("C-c m a" . mc/mark-all-like-this)))
+
+(use-package dotenv-mode
+  :ensure t
+  :mode (("\\.env\\..*\\'" . dotenv-mode)
+	 ("\\.env\\'" . dotenv-mode)))
+
+(use-package diff-hl
+  :ensure t
+  :hook ((after-init . global-diff-hl-mode)
+	 (dired-mode . diff-hl-dired-mode)
+	 (magit-pre-refresh-hook . diff-hl-magit-pre-refresh)
+	 (magit-post-refresh . diff-hl-magit-post-refresh))
+  :config
+  (diff-hl-flydiff-mode +1)
+  (diff-hl-margin-mode +1))
+
+(use-package try
+  :ensure t)
