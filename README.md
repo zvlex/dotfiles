b01ea42280e2ae7cb81236db8b54b4ca8a0a24ed
Aleksandre Zutikov <zvlex.dev@gmail.com>
2024-06-21 22:19:14 +0400

    Update Emacs config and README

--- README.md
+++ README.md
@@ -6,3 +6,16 @@ Fedora Linux
 ```bash
 sudo dnf install git stow emacs neovim vim
 ```
+
+### Install:
+```bash
+git clone git@github.com:zvlex/dotfiles.git ~/.dotfiles
+```
+
+### Usage:
+
+Will symlink config files
+
+```bash
+stow emacs
+```
--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -16,7 +16,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(org-bullets toc-org yaml-mode whitespace-cleanup-mode which-key web-mode try string-inflection smartparens rspec-mode racket-mode projectile-rails pgmacs pg-el pg pdf-tools nov neotree multiple-cursors move-text mode-icons mix magit go-mode exec-path-from-shell erlang emmet-mode elixir-ts-mode elixir-mode diff-hl dashboard company comment-dwim-2 bundler ample-theme all-the-icons)))
+   '(markdown-mode org-bullets toc-org yaml-mode whitespace-cleanup-mode which-key web-mode try string-inflection smartparens rspec-mode racket-mode projectile-rails pgmacs pg-el pg pdf-tools nov neotree multiple-cursors move-text mode-icons mix magit go-mode exec-path-from-shell erlang emmet-mode elixir-ts-mode elixir-mode diff-hl dashboard company comment-dwim-2 bundler ample-theme all-the-icons)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
