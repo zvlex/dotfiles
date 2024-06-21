93ff943e2c7565b94c58e8c25354393952574b54
Aleksandre Zutikov <zvlex.dev@gmail.com>
2024-06-21 21:58:04 +0400

    Dotfiles managed my GNU Stow

---
+++ README.md
@@ -0,0 +1,8 @@
+# Dotfiles
+
+### Dependencies
+Fedora Linux
+
+```bash
+sudo dnf install git stow emacs neovim vim
+```
---
+++ emacs/.emacs.d/init.el
@@ -0,0 +1,25 @@
+;; Require packages
+(require 'package)
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+(package-initialize)
+
+;; Load Emacs Org config file
+(org-babel-load-file
+ (expand-file-name
+  "config.org"
+  user-emacs-directory))
+
+;; Meta
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   '(org-bullets toc-org yaml-mode whitespace-cleanup-mode which-key web-mode try string-inflection smartparens rspec-mode racket-mode projectile-rails pgmacs pg-el pg pdf-tools nov neotree multiple-cursors move-text mode-icons mix magit go-mode exec-path-from-shell erlang emmet-mode elixir-ts-mode elixir-mode diff-hl dashboard company comment-dwim-2 bundler ample-theme all-the-icons)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
